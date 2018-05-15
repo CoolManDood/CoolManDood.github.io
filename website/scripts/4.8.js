cca31be9424a21497b3429b654ff1afa8e45f6ec
Samuel Roush <samuel.roush@warriorlife.net>
2018-05-15 14:11:24 -0700

    committing website folder

---
+++ website/scripts/4.8.js
@@ -0,0 +1,4 @@
+// JavaScript Document
+var sports = "athletics";
+var sleepy = "tired";
+var academics = "classes"
---
+++ website/scripts/greeting.js
@@ -0,0 +1,5 @@
+// JavaScript Document
+
+function greeting() {
+		alert("i'm fine, how are u");
+}
